32a937ecd1bc418177f128bb348bc52d3bfce725
Cristian Pérez Corral <69525230+cperezln@users.noreply.github.com>
2022-10-17 17:28:58 +0200

    Update prac1_2.py

--- Practica1/prac1_2.py
+++ Practica1/prac1_2.py
@@ -38,24 +38,44 @@ def fSimpson(f,xi):  #xi es el conjunto de puntos (3) [x0,x1,x2]
     return sol, abs(sol-a) / a
 
 #A (NO SE A QUE EJEMPLO HACER REFERENCIA)
+g = x**3 - 2*x**2 + 1
+a = 0
+b = 2
+xi = [0, 1, 2]
+print("A) FORMULA RECTANGULO EXTREMO DERECHO")
+s1, es1 = fRectanguloExtDcho(g,a,b)
+print(s1, es1)
+print("A) FORMULA RECTANGULO EXTREMO IZQUIERDO")
+s2, es2 = fRectanguloExtIzdo(g,a,b)
+print(s2, es2)
+print("A) FORMULA RECTANGULO PUNTO MEDIO")
+s3, es3 = fRectanguloMedio(g,a,b)
+print(s3, es3)
+print("A) FORMULA TRAPECIO")
+s4, es4 = fTrapecio(g,a,b)
+print(s4, es4)
+print("A) FORMULA SIMPSON 1/3")
+s5, es5 = fSimpson(g,xi)
+print(s5, es5)
+
 
 #B
 f= cos(x**2-1)
 a = 0
 b = 2*pi
 xi = [0,pi,pi*2]
-print("FORMULA RECTANGULO EXTREMO DERECHO")
+print("B) FORMULA RECTANGULO EXTREMO DERECHO")
 s1, es1 = fRectanguloExtDcho(f,a,b)
 print(s1, es1)
-print("FORMULA RECTANGULO EXTREMO IZQUIERDO")
+print("B) FORMULA RECTANGULO EXTREMO IZQUIERDO")
 s2, es2 = fRectanguloExtIzdo(f,a,b)
 print(s2, es2)
-print("FORMULA RECTANGULO PUNTO MEDIO")
+print("B) FORMULA RECTANGULO PUNTO MEDIO")
 s3, es3 = fRectanguloMedio(f,a,b)
 print(s3, es3)
-print("FORMULA TRAPECIO")
+print("B) FORMULA TRAPECIO")
 s4, es4 = fTrapecio(f,a,b)
 print(s4, es4)
-print("FORMULA SIMPSON 1/3")
+print("B) FORMULA SIMPSON 1/3")
 s5, es5 = fSimpson(f,xi)
 print(s5, es5)
