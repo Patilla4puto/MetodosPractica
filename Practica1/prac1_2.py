317d19644fd60b42d3f0d6c246d33f255e4b3041
Cristian Pérez Corral <69525230+cperezln@users.noreply.github.com>
2022-10-17 17:18:51 +0200

    Update prac1_2.py

--- Practica1/prac1_2.py
+++ Practica1/prac1_2.py
@@ -1,46 +1,61 @@
+#import matplotlib.pyplot as plt
+import numpy
 from sympy import *
 import math
+from sympy.plotting import plot
+
 x = Symbol('x')
 
 #1.- FÓRMULAS RECTANGULARES
 def fRectanguloExtIzdo(f,a,b):
-    return f.subs(x,a)*(b-a)
+    i = N(integrate(f,(x,a,b)))
+    sol = N(f.subs(x,a)*(b-a))
+    return sol, abs(sol-i)/i
 def fRectanguloExtDcho(f,a,b):
-    return f.subs(x,b)*(b-a)
+    i = N(integrate(f,(x,a,b)))
+    sol = N(f.subs(x,b)*(b-a))
+    return [sol, abs(sol - i) / i]
 def fRectanguloMedio(f,a,b):
-    return f.subs(x,(a+b)/2)*(b-a)
+    i = N(integrate(f,(x,a,b)))
+    sol = N(f.subs(x,(a+b)/2)*(b-a) * (b - a))
+    return [sol, abs(sol - i) / i]
 
 #2.- FÓRMULA DEL TRAPECIO
 def fTrapecio(f,a,b):
-    return (b-a)*(f.subs(x,a)+f.subs(x,b))/2
+    i = N(numpy.trapz([a, b]))
+    sol = N((b-a)*(f.subs(x,a)+f.subs(x,b))/2)
+    return sol, abs(sol - i) / i
 
 #3.- FÓRMULA DE SIMPSON 1/3
 def fSimpson(f,xi):  #xi es el conjunto de puntos (3) [x0,x1,x2]
+    a = N(integrate(f, (x, xi[0], xi[2])))
     D = 6
     c = [1,4,1]
     ret = 0
     for i in range(3):
         ret += c[i]*f.subs(x,xi[i])
-    return ret*(xi[2]-xi[0])/D
+    sol = N(ret*(xi[2]-xi[0])/D)
+    return sol, abs(sol-a) / a
 
 #A (NO SE A QUE EJEMPLO HACER REFERENCIA)
 
 #B
 f= cos(x**2-1)
 a = 0
-b = math.pi*2
-xi = [0,math.pi,math.pi*2]
+b = 2*pi
+xi = [0,pi,pi*2]
 print("FORMULA RECTANGULO EXTREMO DERECHO")
-print(N(fRectanguloExtDcho(f,a,b)))
+s1, es1 = fRectanguloExtDcho(f,a,b)
+print(s1, es1)
 print("FORMULA RECTANGULO EXTREMO IZQUIERDO")
-print(N(fRectanguloExtIzdo(f,a,b)))
+s2, es2 = fRectanguloExtIzdo(f,a,b)
+print(s2, es2)
 print("FORMULA RECTANGULO PUNTO MEDIO")
-print(N(fRectanguloMedio(f,a,b)))
+s3, es3 = fRectanguloMedio(f,a,b)
+print(s3, es3)
 print("FORMULA TRAPECIO")
-print(N(fTrapecio(f,a,b)))
+s4, es4 = fTrapecio(f,a,b)
+print(s4, es4)
 print("FORMULA SIMPSON 1/3")
-print(N(fSimpson(f,xi)))
-print("INTEGRAL CON FUNCION TRAPZ")
-print(N(integrate(f,(x,a,b))))
-#   QUEDAN LOS ERRORES RELATIVOS JEJE
-
+s5, es5 = fSimpson(f,xi)
+print(s5, es5)
