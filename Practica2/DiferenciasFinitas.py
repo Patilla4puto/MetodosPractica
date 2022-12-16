cb82ef47e621a1fe3f7fa596259cbaf7a64d59b9
Juan <ja.gordillo.2018@alumnos.urjc.es>
2022-12-16 12:04:44 +0100

    Comentado

--- Practica2/DiferenciasFinitas.py
+++ Practica2/DiferenciasFinitas.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sympy.functions import exp
 
 x= symbols('x')
-
+#Copiamos el codigo de Gauss-Seidel
 def convergencia(e,x_k,x_k1):
     i=0
     conv = True
@@ -34,21 +34,25 @@ def Gauss_Seidel(A,b,x_0,e):
     return x_k
 
 
-# coef = [D, v, q]
+# coef = [D, v, q],
 def condicionesContorno(star, stop, step, coef, f_x, u_0, u_n):
+    #Obtenemos el alpha,beta , gamma y el intervalo de discretización
     alpha = -coef[0]/step**2-coef[1]/(2*step)
     beta = 2*coef[0]/step**2+coef[2]
     gamma =  -coef[0]/step**2+coef[1]/(2*step)
     inter = np.arange(star,stop+step,step)
+    #Si tiene tamaño 3 sustituimos directamente
     if (len(inter) == 3):
         f_1 = f_x.subs({'x':inter[1]})
         u_1= (f_1 -alpha*u_0-gamma*u_n)/beta
         return [u_0,u_1,u_n],inter
     else:
+        #Si no, definimos la matriz A y el vector b y les damos valores
         n = len(inter)
         A = np.zeros((n,n))
         b = []
         for i in range(n):
+            #Si estamos en el primer/ultimo valor asignamos 1 a A y u_o o u_n depende en que extremos estemos
             if(i == 0 or i == n-1):
                 A[i,i]=1
                 if(i==0):
@@ -56,47 +60,53 @@ def condicionesContorno(star, stop, step, coef, f_x, u_0, u_n):
                 else:
                     b.append(u_n)
             else:
+                #Por otro lado asignamos a la diagonal principal y a las secundarias los valores de alpha,betta y gamma
                 A[i,i] = beta
                 A[i,i-1] = alpha
                 A[i,i+1] = gamma
+                #Y en la misma posición añadimos a b el resultado de sustituir xi en f_x
                 b.append(f_x.subs({'x':inter[i]}))
+        #Inicializamos x_i
         u_arr = np.zeros(n)
+        print(A,b)
+        #Devolvemos el resultado de resolver el sistema A*b=u_arr
         return Gauss_Seidel(A, b, u_arr, 1/1000), inter
 
 if __name__ == '__main__':
 
     #Ejercicio 1
+    #Definimos los coeficientes que componen nuestro problema -D, V y q, las condiciones en los extremos y la f_x
     coef = [1, 1, 0]
     u_0 = 0
     u_n = 1
     f_x = 0*x
-    sol_aprox, inter = condicionesContorno(0, 1, 0.5, coef, f_x, u_0, u_n)
+
+    #Solución real
     f_real = (exp(x)-1)/(exp(1)-1)
-    sol_real = [0, N(f_real.subs({'x':0.5})), 1]
-    error_relativo = abs(sol_aprox[1] - sol_real[1])
-    print('Error: {}'.format(error_relativo))
-    plt.plot(inter,sol_real,label = "real")
-    plt.plot(inter, sol_aprox, label="aproximada")
-    plt.legend()
-    plt.show()
-    '''for i in range(10):
+    #Realizamos pruebas con pasos de discretización más pequeños para obserbar la convergencia
+    for i in range(10):
         step = 1/(i+3)
-        sol_aprox, inter = difFinitas(0, 1, step, coef, f_x, u_0, u_n)
+        sol_aprox, inter = condicionesContorno(0, 1, step, coef, f_x, u_0, u_n)
         sol_real = [N(f_real.subs({'x': j})) for j in np.arange(0, 1 + step, step)]
+        error_relativo = abs(sol_aprox[1] - sol_real[1]) / sol_real[1]
+        print('Error: {}'.format(error_relativo))  # Mostramos el error relativo
         plt.plot(inter, sol_real, label="real")
         plt.plot(inter, sol_aprox, label="aproximada")
         plt.legend()
-        plt.show()'''
+        plt.show()
 
     #Ejercicio 2
+
+    #COo en el anterior definimos los  coeficientes que componen nuestro problema -D, V y q, las condiciones en los extremos y la f_x
     coef = [-1, 0, -0.01]
     f_x = -0.01*20 + 0*x
     t_0 = 40
     t_n = 200
+    #Ahora aplicamos la misma secuencia aplicada en el apartado anterior para diferentes valores de discretización y lo mostramos
     for i in range(10):
         step = 10/(i+2)
         sol_aprox, inter = condicionesContorno(0, 10, step, coef, f_x, t_0, t_n)
-        print(sol_aprox, inter)
+
         plt.plot(inter, sol_aprox, label="aproximada")
         plt.legend()
         plt.show()
--- Practica2/EDP_Eliptico.py
+++ Practica2/EDP_Eliptico.py
@@ -1,39 +1,47 @@
 import numpy as np
 from sympy import *
 import matplotlib.pyplot as plt
-from sympy.functions import exp
-
+"""
+Función que nos devuelve la Matriz A y b del problema asociaso al problema eliptico que se nos plantea,
+con tamaños de distretizacion nxn
+"""
 def matriz(n,h):
-
+    #Inicializamos las matrices a ceros
     n2 = int(n**2)
-
     A= np.zeros((n2,n2))
     b= np.zeros(n2)
     for i in range(n2):
         for j in range(n2):
             di = i//n
+            #Hacemos que las matrices correspondientes a las variables i= 1, i=n-1 sean diagonales
             if di==0 or di==n-1:
                 if(i==j):
-
                     A[i,j]=1
             else:
+                #Recabamos información de con que variable estamos tratando con los restos y divisores enteros de las filas y columnas
                 ri = i%n
                 dj = j//n
                 rj = j%n
+                #Damos valor a las varibles en las digonales secundarias, donde solo sus elementos diagonales con ri distinto de 0 y n-1
+                #son distintos de 0
                 if((ri!=0.0 and ri!=n-1) and rj==ri and (di==dj-1 or di == dj+1  )):
 
                     A[i,j]=1
-                if (di==dj  ):
+                #Aquí nos fijamos en las submatrices de la diagonal principal
+                if (di==dj ):
+                    #Los elementos ri =0,n-1 los inicializamos a 1
                     if((rj == 0.0 and ri==0.0)  or (ri==n-1 and rj == n-1 )   ):
-
                         A[i,j]=1
+                    #Aquí solo inicilizamos los elementos de la diagonal (no incluidos en la condición anterior y los de la diagonal de esa misma fila
                     elif (ri == rj):
                         A[i,j]= -4
                         A[i,j-1]=1
-
                         A[i,j+1]=1
+                        #Aprovechamos esta condición e inicializamos la fila corresponiente de b con el valor (pues es en esta fila donde tenemos la ecuación correspondiente
+                        # al problema
                         b[i]=-2*h**2
     return A,b
+#Reutilizamo el codigo de Gauss-Seidel
 def convergencia(e,x_k,x_k1):
     i=0
     conv = True
@@ -42,7 +50,6 @@ def convergencia(e,x_k,x_k1):
             conv = False
         i+=1
     return conv
-
 def Gauss_Seidel(A,b,x_0,e):
     #De momento  es igual a jacobi
     x_k1 = np.copy(x_0)+2*e
@@ -62,19 +69,28 @@ def Gauss_Seidel(A,b,x_0,e):
         k+=1
 
     return x_k
+
+# Función que devuelve el vector de los xi,j del problema eliptico que se nos pide pasandole los limites de definición y los pasos de discretización
 def elipctica(paso,start,stop):
-    n = abs(start - stop) / paso + 1
 
-    A,b = matriz(n,paso)
-    xi_j= np.zeros(int(n*n))
-    return(Gauss_Seidel(A,b,xi_j,1e-4),int(n))
-start,stop,step =-1,1,1/4
-inter = np.arange(start,stop+step,step)
+    n = abs(start - stop) / paso + 1            #Obtenemos el numero de puntos
+    A,b = matriz(n,paso)                        #Obtenemos la matriz A y el vector b de la función que hemos definido al principio
+    xi_j= np.zeros(int(n*n))                    #Inicializamos el vector x_i_j con 0's
+    return(Gauss_Seidel(A,b,xi_j,1e-4),int(n))  #Resolvemos con Gauss-Seidel el sistema A*x_i_j=b y devolvemos el resultado
+start,stop =-1,1
 
-mesh,n =elipctica(step,start, stop)
-mesh=mesh.reshape(n,n)
-X, Y = np.meshgrid(inter, inter)
-print(mesh)
-ax = plt.figure().add_subplot(projection='3d')
-ax.plot_surface(X,Y,mesh)
-plt.show()
+#Realizamos 8 pruebas con casos de discretizacion de entre 3 a 11 puntos
+for step,limit in [((stop-start)/n,n) for n in range(2,11)]:
+    #Aqui suele haber un error y aveces se crean intervalos de n+1 puntos, suponemos que debido a la precisión de float
+    inter = np.arange(start,stop+step,step)
+    #Corregimos el problema de arange
+    if (len(inter)>limit):
+        inter = inter[0:limit+1]
+
+    mesh,n =elipctica(step,start, stop) #Recuperamos los puntos z y el tamaño de discretización
+    #Realizamos los pasos necesarios para mostrar la gráfica
+    mesh=mesh.reshape(n,n)
+    X, Y = np.meshgrid(inter, inter)
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot_surface(X,Y,mesh)
+    plt.show()
